59c9dd1978c09e792741204e516780fa6663164d
nvrcght <av.pogosyan@gmail.com>
2016-04-26 16:40:28 -0400

    Added files via upload

---
+++ SVM/svm_author_id.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+""" 
+    this is the code to accompany the Lesson 2 (SVM) mini-project
+
+    use an SVM to identify emails from the Enron corpus by their authors
+    
+    Sara has label 0
+    Chris has label 1
+
+"""
+import site
+site.addsitedir('/Users/Anush/anaconda/lib/python2.7/site-packages')
+import sys
+from time import time
+#sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+from sklearn import svm
+clf = svm.SVC(kernel = 'linear')
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(labels_test,pred)
+print acc
